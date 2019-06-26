3054260486df5fc80b3c72a11c29d9612754f277
marcus pianco <marcus.ufal@gmail.com>
2019-06-26 00:53:07 -0300

    Build User model and database connection

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Olá' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Marcus',
+    email: 'marcusjrdev@gmail.com',
+    password_hash: '12345',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
