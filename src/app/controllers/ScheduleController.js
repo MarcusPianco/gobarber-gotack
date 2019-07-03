daf4ddf84f5b376dc7c2f4e7a789a0bdd1bfbb59
marcus pianco <marcus.ufal@gmail.com>
2019-07-03 00:01:27 -0300

    feature appointment

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,35 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointments from '../models/Appointments';
+import User from '../models/User';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'user is not provider' });
+    }
+
+    const { date } = req.query;
+    const parserDate = parseISO(date);
+
+    const appointments = await Appointments.findAll({
+      // pegar a primeira hora do dia at´´a hora setada aqui
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parserDate), endOfDay(parserDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import authMiddleware from './app/middlewares/auth';
 import multerConf from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConf);
@@ -18,7 +20,11 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
